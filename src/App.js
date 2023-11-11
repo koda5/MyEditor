0f24437744c6d319fdc55d270e979e8d24243148
koda5 <lkodovski@gmail.com>
2023-11-11 11:32:10 +0100

    pak

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+import { EditorState, ContentState, convertFromHTML } from "draft-js";
+import { Editor } from "react-draft-wysiwyg";
+import { convertToHTML } from "draft-convert";
+import DOMPurify from "dompurify";
+
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import "./App.css";
+import MyEditor from "./MyEditor";
+
+const RecordDefault = {
+  vaText:
+    "<p>Ova e bold <strong>boldirano</strong>, a ova e <i>italizirano</i>.</p></br> <p>A ova e nov red<p/>",
+};
 
 function App() {
+  const [editorState, setEditorState] = useState(() =>
+    EditorState.createEmpty()
+  );
+
+  const [convertedContent, setConvertedContent] = useState(null);
+
+  useEffect(() => {
+    let html = convertToHTML(editorState.getCurrentContent());
+    setConvertedContent(html);
+  }, [editorState]);
+
+  function createMarkup(html) {
+    return {
+      __html: DOMPurify.sanitize(html),
+    };
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Rich Text Editor Example</header>
+
+      <MyEditor record={RecordDefault} />
+      <div
+        className="preview"
+        dangerouslySetInnerHTML={createMarkup(convertedContent)}
+      ></div>
     </div>
   );
 }
---
+++ src/MyEditor.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import { EditorState, ContentState, convertFromHTML } from "draft-js";
+import { Editor } from "react-draft-wysiwyg";
+import "./App.css";
+
+const MyEditor = ({ record }) => {
+  const [editorState, setEditorState] = useState(() =>
+    EditorState.createEmpty()
+  );
+
+  useEffect(() => {
+    if (record && record.vaText) {
+      setTextInEditor(record.vaText);
+    }
+  }, [record]);
+
+  const setTextInEditor = (htmlText) => {
+    const contentState = convertFromHTML(htmlText);
+    const state = ContentState.createFromBlockArray(contentState.contentBlocks);
+    const newEditorState = EditorState.createWithContent(state);
+    setEditorState(newEditorState);
+  };
+
+  return (
+    <div>
+      <Editor
+        editorState={editorState}
+        onEditorStateChange={setEditorState}
+        wrapperClassName="wrapper-class"
+        editorClassName="editor-class"
+        toolbarClassName="toolbar-class"
+        toolbar={{
+          options: ["inline", "blockType"],
+        }}
+      />
+    </div>
+  );
+};
+
+export default MyEditor;
